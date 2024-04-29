7e35e28f1aae8121450a70449513594cd08777ff
Daniel da Rocha <daniel.darocha@birdeatsbug.com>
2024-04-29 19:38:19 +0200

    add iconify-icon web component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
 import "../css/app.css";
+import "iconify-icon";
 
 import { createApp, h } from "vue";
 import { createInertiaApp } from "@inertiajs/vue3";
@@ -15,7 +16,7 @@ createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob("./Pages/**/*.vue")
+            import.meta.glob("./Pages/**/*.vue"),
         ),
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
